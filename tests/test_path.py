5b298afacae2a5bf5d851505fd817fed0c9ac698
Christoph Dörrer <d-chris@web.de>
2025-05-18 22:41:13 +0200

    parametrized tests for path

--- tests/test_path.py
+++ tests/test_path.py
@@ -1,29 +1,60 @@
+import sys
+from pathlib import Path
+
 import click
+import pytest
 from click.testing import CliRunner
 
 import clicktypes
 
 
-@click.command(name="path", help="path validator")
-@click.argument(
-    "path",
-    type=clicktypes.Path(resolve_path=True, executable=True),
+@pytest.mark.parametrize(
+    "value,kwargs,expected",
+    [
+        [
+            ("python"),
+            {
+                "resolve_path": True,
+                "executable": True,
+            },
+            0,
+        ],
+        [
+            ("python2"),
+            {
+                "resolve_path": True,
+                "executable": True,
+            },
+            1,
+        ],
+        [
+            ("python"),
+            {},
+            1,
+        ],
+        [
+            (Path(sys.executable).as_posix()),
+            {},
+            0,
+        ],
+    ],
+    ids=repr,
 )
-def cli(path):
+def test_path(value, kwargs, expected):
 
-    if path.exists():
-        click.echo(f"valid {path=}")
-    else:
-        click.echo(f"invalid {path=}", err=True)
-        raise SystemExit(1)
+    @click.command(name="path", help="path validator")
+    @click.argument(
+        "path",
+        type=clicktypes.Path(**kwargs),
+    )
+    def cli(path: Path) -> None:
 
-
-def test_path():
+        if path.is_file():
+            click.echo(f"valid {path=}")
+        else:
+            click.echo(f"invalid {path=}", err=True)
+            raise SystemExit(1)
 
     runner = CliRunner()
-    result = runner.invoke(cli, ["python"])
-    assert result.exit_code == 0
-
-
-if __name__ == "__main__":
-    cli()
+    result = runner.invoke(cli, value)
+    assert result.exit_code == expected
